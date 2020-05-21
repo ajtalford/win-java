c7253c420b2806d90b85c3bcde3e8633d42238e4
aljertal <aljertal@gmail.com>
2020-05-21 07:31:28 -0400

    complete Magazine and NewsPaper constructors, get/set, and props

--- library-management-system/src/Magazine.java
+++ library-management-system/src/Magazine.java
@@ -2,10 +2,32 @@
 
 
 public class Magazine extends Periodical {
+    // Magazine properties specific to magazine
+    private String advertise;
+    private String coverPersonality;
+    // Magazine constructor inheriting methods and properties from Periodical
+    public Magazine(String ISBN, String genre, String author, String binding, double rentCost,
+                    double retailPrice, int publishedYear,
+                    String topic, boolean series, String advertise, String coverPersonality) {
+        super(ISBN, genre, author, binding, rentCost, retailPrice, publishedYear, topic, series);
+        this.advertise = advertise;
+        this.coverPersonality = coverPersonality;
+    }
 
 
+    public String getAdvertise() {
+        return advertise;
+    }
 
+    public void setAdvertise(String advertise) {
+        this.advertise = advertise;
+    }
 
+    public String getCoverPersonality() {
+        return coverPersonality;
+    }
 
-
+    public void setCoverPersonality(String coverPersonality) {
+        this.coverPersonality = coverPersonality;
+    }
 }
--- library-management-system/src/NewsPaper.java
+++ library-management-system/src/NewsPaper.java
@@ -1,10 +1,26 @@
 // Magazine and NewsPaper are Derived from Periodical
 
 
+import java.util.Date;
+
 public class NewsPaper extends Periodical {
+    // property for date printed
+    private Date printed;
 
+    public NewsPaper(String ISBN, String genre, String author, String binding, double rentCost,
+                     double retailPrice, int publishedYear, String topic,
+                     boolean series, Date printed) {
+        super(ISBN, genre, author, binding, rentCost, retailPrice, publishedYear, topic, series);
+        this.printed = printed;
+    }
 
+    public Date getPrinted() {
+        return printed;
+    }
 
-
+    // getter and setter for printed date
+    public void setPrinted(Date printed) {
+        this.printed = printed;
+    }
 
 }
