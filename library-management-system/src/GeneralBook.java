595f0c4faf80dd45855a9c701ce32ece100d095c
aljertal <aljertal@gmail.com>
2020-05-21 06:23:37 -0400

    have GeneralBook class extend Book class

--- library-management-system/src/GeneralBook.java
+++ library-management-system/src/GeneralBook.java
@@ -1,6 +1,9 @@
 // ReferenceBook and GeneralBook derived from books
-
-public class GeneralBook {
-
-
+//GeneralBook class extending Book class
+public class GeneralBook extends Book {
+    // GeneralBook constructor inheriting all of the methods and properties of book
+    public GeneralBook(String ISBN, String genre, String author, String binding, double rentCost,
+                       double retailPrice, int publishedYear) {
+        super(ISBN, genre, author, binding, rentCost, retailPrice, publishedYear);
+    }
 }
